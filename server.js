d8ff1717dafa33eaa3e5678369c2757b4e1fd296
haleywitherell <hwitherell22@gmail.com>
2021-10-19 21:40:22 -0400

    js add functions

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const { allowedNodeEnvironmentFlags } = require('process');
-const Connection = require('mysql2/typings/mysql/lib/Connection');
-// const Connection = require('mysql2/typings/mysql/lib/Connection');
+// const cTable = require('console.table')
+// require('dotenv').config()
 
 const db = mysql.createConnection({
     host:"localhost",
@@ -39,12 +38,10 @@ function askQuestions(){
             addRole()
         } else if(choice == "Add Employee") {
             addEmployee()
-        } else {
+        } else if(choice == "Quit") {
             connection.end()
         }; 
-            
-        
-        // add onto the if statement with othet tabels that I added above 
+       
     })
 }
 
@@ -64,7 +61,83 @@ function viewFunction(table){
 }
 
 
-// create functions for query 21 and 22
-function addDepartment(table){
+// add deptartment function
+function addDepartment(){
 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Please insert the name of the department',
+            
+        },
+    ]) .then((data) => {
+        db.query(`INSERT INTO department(name) VALUES( ? )`, data.department)
+
+        askQuestions();
+    })
+}
+
+// add role function
+function addRole(){
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of the role?',
+            
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary for this role?',
+            
+        },
+        {
+            type: 'input',
+            name: 'department_id ',
+            message: 'What department is the role in?',
+            
+        },
+    ]) .then((data) => {
+        db.query(`INSERT INTO roles VALUES ?`, data.roles)
+
+        askQuestions();
+    })
+}
+
+// add employee function
+function addEmployee(){
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the employees first name?',
+            
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the employees last name?',
+            
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'List the employees role',
+            
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'List the employees manager',
+            
+        },
+    ]) .then((data) => {
+        db.query(`INSERT INTO roles VALUES ?`, data.roles)
+
+        askQuestions();
+    })
 }
